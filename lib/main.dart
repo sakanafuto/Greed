f35b235e5b4d2a09a004db3821c67fd7e22bee6f
sakanafuto <sakanafuto765@gmail.com>
2022-06-21 08:27:16 +0900

    feat: first commit

---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: [
+              // Icon(Icons.location_searching_sharp),
+              // SizedBox(width: 10),
+              Text("Flutter学習"),
+              Spacer(),
+              Icon(Icons.bakery_dining_rounded),
+            ],
+          )
+        ),
+
+        drawer: Drawer(child: Center(child: Text("Drawer"))),
+
+        body: Container(
+          margin: EdgeInsets.all(40.0),
+          padding: EdgeInsets.only(right: 10.0, left: 10.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.lightGreen.shade500,
+              width: 3.0,
+            ),
+            borderRadius: BorderRadius.circular(50),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 20,
+              )
+            ]
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text(
+                "First text.",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.green.shade900,
+                  fontWeight: FontWeight.bold,
+                )
+              ),
+              Padding(
+                padding: EdgeInsets.all(32.0),
+                child: Text("padding!!"),
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.lightBlue[50],
+                padding: EdgeInsets.all(5.0),
+                margin: EdgeInsets.all(10.0),
+                child: Icon(Icons.margin),
+              ),
+              Text("Second text."),
+              TextButton(
+                onPressed: () => {print("button.")},
+                child: Text("update."),
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                Icon(
+                  Icons.favorite,
+                  color: Colors.pink[600],
+                  size: 24.0,
+                ),
+                Icon(
+                  Icons.beach_access,
+                  color: Colors.blue[600],
+                  size: 36.0,
+                ),
+                Icon(Icons.audiotrack, color: Colors.green[600], size: 30.0),
+              ])
+            ]
+          )
+        ));
+  }
+}
