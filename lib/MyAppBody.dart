11552e3cc9bf53b0948987daff1263a2544e8e31
sakanafuto <sakanafuto765@gmail.com>
2022-06-23 21:36:06 +0900

    feat: TodoListPageの作成

--- lib/MyAppBody.dart
+++ lib/MyAppBody.dart
@@ -1,34 +1,35 @@
 import 'package:flutter/material.dart';
 
 class MyAppBody extends StatelessWidget {
-  MyAppBody({Key? key, this.changeTheme}) : super(key: key);
-  final changeTheme;
+  const MyAppBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(40.0),
-      padding: EdgeInsets.only(right: 10.0, left: 10.0),
-
-      // decoration: BoxDecoration(
-      //     color: Colors.white,
-      //     border: Border.all(
-      //       color: Colors.lightGreen.shade500,
-      //       width: 3.0,
-      //     ),
-      //     borderRadius: BorderRadius.circular(50),
-      //     boxShadow: [
-      //       BoxShadow(
-      //         blurRadius: 20,
-      //       )
-      //     ]),
-      // child: Center(
-      //     child: ElevatedButton(
-      //   onPressed: changeTheme,
-      //   child: Text("Toggle theme"),
-      // )),
-    
-      child: Text("sss"),
-    );
+        margin: const EdgeInsets.all(40.0),
+        padding: const EdgeInsets.all(80.0),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.lightGreen.shade500,
+              width: 3.0,
+            ),
+            borderRadius: BorderRadius.circular(50),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 20,
+              )
+            ]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Text("sss"),
+            Row(
+              children: [Text("lskdjf"), Text("Donkey Kong")],
+            )
+            // CircularProgressIndicator(),
+            // LinearProgressIndicator(),
+          ],
+        ));
   }
 }
--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'MyAppBody.dart';
+import 'ToDoListPage.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
@@ -15,100 +16,126 @@ class _MyAppState extends State<MyApp> {
     setState(() => dark = !dark);
   }
 
-  // final pages = [
-  //   Container(
-  //     color: Colors.pink.shade500,
-  //   ),
-  //   Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: <Widget>[
-  //     Text("First text.",
-  //         style: TextStyle(
-  //           fontSize: 24,
-  //           color: Colors.green.shade900,
-  //           fontWeight: FontWeight.bold,
-  //         )),
-  //     Padding(
-  //       padding: EdgeInsets.all(32.0),
-  //       child: Text("padding!!"),
-  //     ),
-  //     Container(
-  //       height: 100.0,
-  //       width: double.infinity,
-  //       color: Colors.lightBlue[50],
-  //       padding: EdgeInsets.all(5.0),
-  //       margin: EdgeInsets.all(10.0),
-  //       child: Icon(Icons.margin),
-  //     ),
-  //     Text("Second text."),
-  //     TextButton(
-  //       onPressed: () => {print("button.")},
-  //       child: Text("update."),
-  //     ),
-  //     Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-  //       Icon(
-  //         Icons.favorite,
-  //         color: Colors.pink[600],
-  //         size: 24.0,
-  //       ),
-  //       Icon(
-  //         Icons.beach_access,
-  //         color: Colors.blue[600],
-  //         size: 36.0,
-  //       ),
-  //       Icon(Icons.audiotrack, color: Colors.green[600], size: 30.0),
-  //     ])
-  //   ]),
-  //   Center(
-  //     child: ElevatedButton(
-  //       onPressed: _changeTheme,
-  //       child: Text("Toggle theme"),
-  //     )
-  //   ),
-  // ];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: dark ? ThemeData.dark() : ThemeData(primarySwatch: Colors.green),
-      home: Scaffold(
-        appBar: AppBar(
+      theme: dark
+          ? ThemeData.dark()
+          : ThemeData(
+              primarySwatch: Colors.teal,
+            ),
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
             title: Row(
-          children: [
-            Text("衝動買い抑制リスト"),
-            Spacer(),
-            Icon(Icons.bakery_dining_rounded),
-          ],
-        )),
-        drawer: Drawer(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text("abc"),
-                      Text("def"),
-                      Text("ghi"),
-                    ]
-                  ),
-                ]
-              ),
-              Padding(
-                  padding: EdgeInsets.only(top: 100.0, right: 160.0),
-                  child: FloatingActionButton(
-                    onPressed: _changeTheme,
-                    child: Icon(Icons.sunny,
-                      color: Colors.amber.shade300, size: 32.0)
+              children: [
+                Text("衝動買いはダメ！"),
+                Spacer(),
+              ],
+            ),
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.face_unlock_rounded),
+                ),
+                Tab(
+                  icon: Icon(
+                    Icons.rocket_launch,
                   ),
                 ),
+                Tab(
+                  icon: Icon(
+                    Icons.table_bar_sharp,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          drawer: Drawer(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const <Widget>[
+                        Text("abc"),
+                        Text("def"),
+                        Text("ghi"),
+                      ]),
+                ]),
+                Padding(
+                  padding: const EdgeInsets.only(top: 100.0, right: 160.0),
+                  child: FloatingActionButton(
+                      onPressed: _changeTheme,
+                      child: Icon(Icons.sunny,
+                          color: Colors.amber.shade300, size: 32.0)),
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Container(
+                color: Colors.yellow.shade200,
+              ),
+              TodoListPage(),
+              MyAppBody(),
             ],
           ),
         ),
-        body: MyAppBody(changeTheme: this._changeTheme),
       ),
     );
   }
 }
+
+// final pages = [
+//   Container(
+//     color: Colors.pink.shade500,
+//   ),
+//   Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: <Widget>[
+//     Text("First text.",
+//         style: TextStyle(
+//           fontSize: 24,
+//           color: Colors.green.shade900,
+//           fontWeight: FontWeight.bold,
+//         )),
+//     Padding(
+//       padding: EdgeInsets.all(32.0),
+//       child: Text("padding!!"),
+//     ),
+//     Container(
+//       height: 100.0,
+//       width: double.infinity,
+//       color: Colors.lightBlue[50],
+//       padding: EdgeInsets.all(5.0),
+//       margin: EdgeInsets.all(10.0),
+//       child: Icon(Icons.margin),
+//     ),
+//     Text("Second text."),
+//     TextButton(
+//       onPressed: () => {print("button.")},
+//       child: Text("update."),
+//     ),
+//     Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+//       Icon(
+//         Icons.favorite,
+//         color: Colors.pink[600],
+//         size: 24.0,
+//       ),
+//       Icon(
+//         Icons.beach_access,
+//         color: Colors.blue[600],
+//         size: 36.0,
+//       ),
+//       Icon(Icons.audiotrack, color: Colors.green[600], size: 30.0),
+//     ])
+//   ]),
+//   Center(
+//       child: ElevatedButton(
+//     onPressed: _changeTheme,
+//     child: Text("Toggle theme"),
+//   )),
+// ];
