118654c5343be421c11cb29ee9c5cdd1cd0c18bc
sakanafuto <sakanafuto765@gmail.com>
2022-06-25 17:05:37 +0900

    feat: todoの追加まで完了

---
+++ lib/favorite_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class GreedAppBody extends StatelessWidget {
+  const GreedAppBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          margin: const EdgeInsets.all(40.0),
+          padding: const EdgeInsets.all(80.0),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Colors.lightGreen.shade500,
+                width: 3.0,
+              ),
+              borderRadius: BorderRadius.circular(50),
+              boxShadow: const [
+                BoxShadow(
+                  blurRadius: 20,
+                )
+              ]),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Text("sss"),
+              Row(
+                children: const [Text("lskdjf")],
+              ),
+              // const CircularProgressIndicator(),
+              // const LinearProgressIndicator(),
+            ],
+          )),
+    );
+  }
+}
---
+++ lib/todo_page.dart
@@ -0,0 +1,98 @@
+import "package:flutter/material.dart";
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  List<String> todoList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                title: Text(todoList[index]),
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final newListText = await Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return TodoAddPage();
+            }),
+          );
+          if (newListText != null) {
+            setState(() {
+              todoList.add(newListText);
+            });
+          }
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TodoAddPage extends StatefulWidget {
+  @override
+  _TodoAddPageState createState() => _TodoAddPageState();
+}
+
+class _TodoAddPageState extends State<TodoAddPage> {
+  String _text = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("欲しいモノを追加"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(_text, style: TextStyle(color: Colors.green.shade800)),
+            TextField(onChanged: (String value) {
+              setState(() {
+                _text = value;
+              });
+            }),
+            const SizedBox(height: 8),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop(_text);
+                },
+                child:
+                    const Text("リスト追加", style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("キャンセル"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
